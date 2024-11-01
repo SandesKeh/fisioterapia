5420622359adfc038a2437e028dc61d20e97bb93
SandesKeh <kevillynsandes07@gmail.com>
2024-11-01 01:45:45 -0300

    continuação do axios

--- src/pages/inventario/index.jsx
+++ src/pages/inventario/index.jsx
@@ -1,8 +1,32 @@
 import './index.scss';
 import Cabecalho from '../../components/cabecalho';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
+
 export default function Inventario(){
+
+    const [mostrarinventario, setMostrarinventario] = useState(false);
+
+    const abririnventario = () => {
+        setMostrarinventario(true);
+    };
+
+    const fecharinventario = (e) => {
+        
+        setMostrarinventario(false)
+        
+    };
+
+    const [nomeproduto, setNomeproduto]= useState('');
+    const [categoria, setCategoria]= useState('');
+    const [estoque, setEstoque]= useState('');
+    const [ondecomprou, setOndecomprou] = useState('');
+    const [unitário, setUnitario] = useState('');
+    const [total, setTotal]= useState('');
+    const [data, setData]= useState('')
+
+    
     return(
         <div className="inventario">
              <div className="protecao">
@@ -79,9 +103,45 @@ export default function Inventario(){
 
                 </table>
                 <div className="botao">
-                    <button> + Adicionar Produto </button>
+                    <button onClick={abririnventario} > + Adicionar Produto </button>
                     <button> Editar </button>
                 </div>
+
+                {mostrarinventario && (
+                        <div className="popup-background">
+                            <div className="popup">
+                                <div className="mensagem">
+                                    <h2>Adicionar produto  </h2>
+                                    <img onClick={fecharinventario} src="/assets/image/bx-x.svg" alt="" />
+                                </div>
+                                <div className="mensage">
+                                    <h2> Nome produto : </h2>
+                                        <input type="text" placeholder='  inserir o nome do produto'  />
+
+                                    <h2> Categoria :</h2>
+                                        <input type="text" placeholder='Pilates ou fisioterapia  '  />
+                                    <h2>Qts. em estoque</h2>
+                                        <input type="text" placeholder='Ex: 20' />
+                                    <h2>Onde comprou </h2>
+                                        <input type="text" placeholder='Amazon' />
+                                    <h2>Preço Unitario </h2>    
+                                        <input type="text" placeholder='R$ 80,32' />
+                                    <h2>Valor total</h2>
+                                        <input type="text" placeholder='R$ 803' />    
+                                    <h2>Data da Compra</h2> 
+                                        <input type="text" placeholder='07/07/2024' />                         
+                                </div>
+                                <div className="botao">
+                                   
+                                    <div className="button">
+                                        <button className='bota' onClick={fecharinventario} > Cancelar </button>
+                                        <button  > Salvar </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    )}
+
             </div>
           
         </div>
